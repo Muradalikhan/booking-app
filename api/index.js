1322b7a3010716a58e86225f8b2d04d6cda82b2b
Murad khan <murad.jaddoon@gmail.com>
2022-10-07 13:03:36 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js"
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
 
 const app = express();
 dotenv.config();
@@ -17,11 +21,25 @@ const connect = async () => {
 mongoose.connection.on("disconnected",()=>{
     console.log("mongoDb disconnected!")
 })
+
+app.use(express.json())
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err,req,res,next)=>{
+    const errorStatus=err.status || 500
+    const errorMessage=err.message || "Something went wrong"
+    return res.status(errorStatus).json({
+        success:false,
+        message:errorMessage,
+        stack:err.stack
+    })
+})
+
 app.listen(8800, () => {
   connect();
   console.log("run on port 8800");
 });
-
-app.get("/",(req,res)=>{
-    res.send("this is the api request")
-})
